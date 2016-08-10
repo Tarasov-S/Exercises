f1365e50584e0cce6683ef65c94373d4010ed27d
Tarasov-S <sm.tarasov@gmail.com>
2016-08-10 21:30:43 +0300

    Add the first exercise 2.1

---
+++ src/thinkinginjava/chapter2/exercise1/DefaultInitFields.java
@@ -0,0 +1,9 @@
+package thinkinginjava.chapter2.exercise1;
+
+class DefaultInitFields {
+    int anInt;              // = 0
+    double aDouble;         // = 0.0
+    char aChar;             // = '\u0000’(null)
+    boolean aBoolean;       // = false
+    String aString;         // = null
+}
---
+++ src/thinkinginjava/chapter2/exercise1/Main.java
@@ -0,0 +1,18 @@
+package thinkinginjava.chapter2.exercise1;
+
+/* Chapter 2. Exercise 1. Difficult - 2/10.
+* Create a class containing an int and a char that are not initialized,
+* and print their values to verify that Java performs default initialization.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        DefaultInitFields fields = new DefaultInitFields();
+
+        System.out.println("int = " + fields.anInt);            // out:  int = 0
+        System.out.println("double = " + fields.aDouble);       // out:  double = 0.0
+        System.out.println("char = " + fields.aChar);           // out:  char =
+        System.out.println("boolean = " + fields.aBoolean);     // out:  boolean = false
+        System.out.println("String = " + fields.aString);       // out:  String = null
+    }
+}
