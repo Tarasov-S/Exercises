7acf667592615035b6b25f3eaafdc55a77d6e0b7
Tarasov-S <sm.tarasov@gmail.com>
2016-08-11 15:49:46 +0300

    Add the exercise 2.3

---
+++ src/thinkinginjava/chapter2/exercise3/ATypeName.java
@@ -0,0 +1,7 @@
+package thinkinginjava.chapter2.exercise3;
+
+class ATypeName {
+    void WhoAreYou() {
+        System.out.println("I am a new type.");
+    }
+}
---
+++ src/thinkinginjava/chapter2/exercise3/Main.java
@@ -0,0 +1,15 @@
+package thinkinginjava.chapter2.exercise3;
+
+/*
+ * Chapter 2. Exercise 3. Difficult - 1/10.
+ *
+ * Find the code fragments involving ATypeName
+ * and turn them into a program that compiles and runs.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        ATypeName aTypeName = new ATypeName();
+        aTypeName.WhoAreYou();      // out: I am a new type.
+    }
+}
