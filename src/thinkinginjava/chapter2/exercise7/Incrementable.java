8db6c1f7812865f016e0c09775f72d23edaa9e88
Tarasov-S <sm.tarasov@gmail.com>
2016-08-11 20:56:00 +0300

    Add the exercise 2.7, again.

---
+++ src/thinkinginjava/chapter2/exercise7/Incrementable.java
@@ -0,0 +1,8 @@
+package thinkinginjava.chapter2.exercise7;
+
+class Incrementable {
+
+    static void increment() {
+        StaticTest.i++;
+    }
+}
---
+++ src/thinkinginjava/chapter2/exercise7/StaticTest.java
@@ -0,0 +1,6 @@
+package thinkinginjava.chapter2.exercise7;
+
+class StaticTest {
+
+    static int i = 47;
+}
