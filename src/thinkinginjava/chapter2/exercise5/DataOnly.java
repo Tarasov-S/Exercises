e4d17fe0a37978ef4660b0907cdf05e313138d6b
Tarasov-S <sm.tarasov@gmail.com>
2016-08-11 16:33:03 +0300

    Add the exercise 2.5

---
+++ src/thinkinginjava/chapter2/exercise5/DataOnly.java
@@ -0,0 +1,8 @@
+package thinkinginjava.chapter2.exercise5;
+
+class DataOnly {
+
+    int i = 47;
+    double d = 1.1;
+    boolean b = false;
+}
---
+++ src/thinkinginjava/chapter2/exercise5/Main.java
@@ -0,0 +1,20 @@
+package thinkinginjava.chapter2.exercise5;
+
+/*
+ * Chapter 2. Exercise 5. Difficult - 1/10.
+ *
+ * Modify the previous exercise so that the values of the data
+ * in DataOnly are assigned to and printed in main( ).
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        DataOnly data = new DataOnly();
+
+        System.out.println(data.i);     // out:  47
+        System.out.println(data.d);     // out:  1.1
+        System.out.println(data.b);     // out:  false
+    }
+
+}
