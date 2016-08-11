49fc5ada463eb25cce6a3e3c73e85be8dc7a1f1e
Tarasov-S <sm.tarasov@gmail.com>
2016-08-11 15:19:22 +0300

    Cosmetic changes in the comments.

--- src/thinkinginjava/chapter2/exercise1/Main.java
+++ src/thinkinginjava/chapter2/exercise1/Main.java
@@ -1,9 +1,11 @@
 package thinkinginjava.chapter2.exercise1;
 
-/* Chapter 2. Exercise 1. Difficult - 2/10.
-* Create a class containing an int and a char that are not initialized,
-* and print their values to verify that Java performs default initialization.
-*/
+/*
+ * Chapter 2. Exercise 1. Difficult - 2/10.
+ *
+ * Create a class containing an int and a char that are not initialized,
+ * and print their values to verify that Java performs default initialization.
+ */
 
 public class Main {
     public static void main(String[] args) {
