eb4ad0d2173a5e25265a00d3b491504b14a58b47
Tarasov-S <sm.tarasov@gmail.com>
2016-08-11 21:18:48 +0300

    Add the exercise 2.9

---
+++ src/thinkinginjava/chapter2/exercise9/AutoBoxing.java
@@ -0,0 +1,23 @@
+package thinkinginjava.chapter2.exercise9;
+
+class AutoBoxing {
+
+    void printPrimitiveInt(int i) {
+        System.out.println(i);
+    }
+
+    void printObjectInt(Integer i) {
+        System.out.println(i);
+    }
+
+    int getPrimitiveInt() {
+        int i = 22;
+        return i;
+    }
+
+    Integer getObjectInt() {
+        Integer i = new Integer(33);
+        return i;
+    }
+
+}
