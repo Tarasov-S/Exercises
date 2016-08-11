e53ecfac2f12ef166761553a02681c7920c27097
Tarasov-S <sm.tarasov@gmail.com>
2016-08-11 16:55:28 +0300

    Add the exercise 2.6

---
+++ src/thinkinginjava/chapter2/exercise6/Main.java
@@ -0,0 +1,18 @@
+package thinkinginjava.chapter2.exercise6;
+
+/*
+ * Chapter 2. Exercise 6. Difficult - 2/10.
+ *
+ * Write a program that includes and calls the storage( ) method
+ * defined as a code fragment in this chapter.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        StorageCalc calc = new StorageCalc();
+
+        int size = calc.storage("The String");
+        System.out.println(size + " bytes");    // out: 20 bytes
+    }
+}
---
+++ src/thinkinginjava/chapter2/exercise6/StorageCalc.java
@@ -0,0 +1,8 @@
+package thinkinginjava.chapter2.exercise6;
+
+class StorageCalc {
+
+    int storage(String s) {
+        return s.length() * 2;
+    }
+}
