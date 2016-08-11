212d742d46ce345b9067fde794df58f21248a628
Tarasov-S <sm.tarasov@gmail.com>
2016-08-11 20:21:13 +0300

    Add the exercise 2.7

---
+++ src/thinkinginjava/chapter2/exercise7/Main.java
@@ -0,0 +1,50 @@
+package thinkinginjava.chapter2.exercise7;
+
+/*
+ * Chapter 2. Exercise 7. Difficult - 1/10.
+ *
+ * Turn the Incrementable code fragments into a working program.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        StaticTest st1 = new StaticTest();
+        StaticTest st2 = new StaticTest();
+        Incrementable sf = new Incrementable();
+
+        System.out.println("StaticTest.i = " + StaticTest.i);   // out: StaticTest.i = 47
+        System.out.println("st1.i = " + st1.i);                 // out: st1.i = 47
+        System.out.println("st2.i = " + st2.i);                 // out: st2.i = 47
+
+        StaticTest.i++;
+
+        System.out.println("StaticTest.i = " + StaticTest.i);   // out: StaticTest.i = 48
+        System.out.println("st1.i = " + st1.i);                 // out: st1.i = 48
+        System.out.println("st2.i = " + st2.i);                 // out: st2.i = 48
+
+        st1.i++;
+
+        System.out.println("StaticTest.i = " + StaticTest.i);   // out: StaticTest.i = 49
+        System.out.println("st1.i = " + st1.i);                 // out: st1.i = 49
+        System.out.println("st2.i = " + st2.i);                 // out: st2.i = 49
+
+        Incrementable.increment();
+
+        System.out.println("StaticTest.i = " + StaticTest.i);   // out: StaticTest.i = 50
+        System.out.println("st1.i = " + st1.i);                 // out: st1.i = 50
+        System.out.println("st2.i = " + st2.i);                 // out: st2.i = 50
+
+        sf.increment();
+
+        System.out.println("StaticTest.i = " + StaticTest.i);   // out: StaticTest.i = 51
+        System.out.println("st1.i = " + st1.i);                 // out: st1.i = 51
+        System.out.println("st2.i = " + st2.i);                 // out: st2.i = 51
+    }
+}
+
+/*
+ * Using the class name is the preferred way to refer to a static variable.
+ * Not only does it emphasize that variable’s static nature,
+ * but in some cases it gives the compiler better opportunities for optimization.
+ */
