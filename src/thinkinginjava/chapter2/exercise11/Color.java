8c98251ab1b5603a64ddae80ddc84a2edd90f8dd
Tarasov-S <sm.tarasov@gmail.com>
2016-08-12 17:41:31 +0300

    Add exercise 2.11

---
+++ src/thinkinginjava/chapter2/exercise11/Color.java
@@ -0,0 +1,14 @@
+package thinkinginjava.chapter2.exercise11;
+
+class Color {
+
+    private int color;
+
+    void changeColor(int newColor) {
+        color = newColor;
+    }
+
+    int getColor() {
+        return color;
+    }
+}
