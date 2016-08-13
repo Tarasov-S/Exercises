aaa5103f50823ac5acd8997f3c4a9875b378fb3c
Tarasov-S <sm.tarasov@gmail.com>
2016-08-13 16:54:17 +0300

    Add the exercise 3.1

---
+++ src/thinkinginjava/chapter03/exercise01/Main.java
@@ -0,0 +1,17 @@
+package thinkinginjava.chapter03.exercise01;
+
+/*
+ * Chapter 3. Exercise 1. Difficult - 1/10.
+ *
+ * Write a program that uses the “short” and normal form of print statement.
+ */
+
+import static thinkinginjava.chapter02.exercise16.Print.print;
+
+public class Main {
+
+    public static void main(String[] args) {
+        print("Hello!");                    // out: Hello!
+        System.out.println("World!");       // out: World!
+    }
+}
---
+++ src/thinkinginjava/chapter03/exercise01/Print.java
@@ -0,0 +1,8 @@
+package thinkinginjava.chapter03.exercise01;
+
+public class Print {
+
+    public static void print(Object object) {
+        System.out.println(object);
+    }
+}
