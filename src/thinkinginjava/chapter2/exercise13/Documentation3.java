11606329135beddbf1e8b4d173b7f2eb1682de67
Tarasov-S <sm.tarasov@gmail.com>
2016-08-13 10:50:09 +0300

    Add exercise 2.13

---
+++ src/thinkinginjava/chapter2/exercise13/Documentation3.java
@@ -0,0 +1,20 @@
+package thinkinginjava.chapter2.exercise13;
+
+/*
+ * Chapter 2. Exercise 13. Difficult - 1/10.
+ *
+ * Run Documentation1.java, Documentation2.java, and Documentation3.java through Javadoc. Verify the resulting
+ * documentation with your Web browser.
+ */
+
+/**
+ * You can <em>even</em> insert a list:
+ * <ol>
+ * <li> Item one
+ * <li> Item two
+ * <li> Item three
+ * </ol>
+ */
+public class Documentation3 {
+
+}
