f5392c0f97e5e4ffbeafe2938945392f4dbf5f40
tarasov-s <sm.tarasov@gmail.com>
2016-08-16 15:14:42 +0300

    Add the exercise 4.1

---
+++ src/thinkinginjava/chapter04/exercise01/Main.java
@@ -0,0 +1,32 @@
+package thinkinginjava.chapter04.exercise01;
+
+/*
+ * Chapter 4. Exercise 1. Difficult - 1/10.
+ *
+ * Write a program that prints values from 1 to 100.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
+/* Out:
+ *
+ * 1
+ * 2
+ * 3
+ * 4
+ *....
+ *....
+ *....
+ * 96
+ * 97
+ * 98
+ * 99
+ * 100
+ */
