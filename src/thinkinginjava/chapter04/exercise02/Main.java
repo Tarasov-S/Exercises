87e433436365ce1d685be1ab06f8c1a5f002c927
tarasov-s <sm.tarasov@gmail.com>
2016-08-16 17:36:04 +0300

    Add the exercise 4.2

---
+++ src/thinkinginjava/chapter04/exercise02/Main.java
@@ -0,0 +1,40 @@
+package thinkinginjava.chapter04.exercise02;
+
+/*
+ * Chapter 4. Exercise 2. Difficult - 2/10.
+ *
+ * Write a program that generates 25 random int values. For each value, use an if-else statement to classify
+ * it as greater than, less than, or equal to a second randomly generated value.
+ */
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int a;
+        int b;
+
+        for (int i = 0; i < 25; i++) {
+            a = random.nextInt(10);
+            b = random.nextInt(10);
+            compare(a, b);
+
+
+        }
+    }
+
+    private static void compare(int a, int b) {
+        String relation;
+
+        if (a < b) {
+            relation = " < ";
+        } else if (a > b) {
+            relation = " > ";
+        } else {
+            relation = " = ";
+        }
+        System.out.println(a + relation + b);
+    }
+}
