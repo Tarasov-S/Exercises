1298aa8f1123971f8f121fc0725129ffcd9b0a3f
tarasov-s <sm.tarasov@gmail.com>
2016-08-18 11:57:13 +0300

    Add the exercise  5.7

---
+++ src/thinkinginjava/chapter05/exercise07/Main.java
@@ -0,0 +1,15 @@
+package thinkinginjava.chapter05.exercise07;
+
+/*
+ * Chapter 5. Exercise 7. Difficult - 1/10.
+ *
+ * Create a class without a constructor, and then create an object of that class in main( ) to verify that
+ * the default constructor is automatically synthesized.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Test test = new Test();              // Constructor exists.
+    }
+}
---
+++ src/thinkinginjava/chapter05/exercise07/Test.java
@@ -0,0 +1,5 @@
+package thinkinginjava.chapter05.exercise07;
+
+public class Test {
+    // Constructor is missing.
+}
