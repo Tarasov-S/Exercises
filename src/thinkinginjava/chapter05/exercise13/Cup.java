48a7ad85728b60dc64d53ea90256d6831bae055e
Sergei Tarasov <sm.tarasov@gmail.com>
2017-11-07 22:31:23 +0200

    Added exercises 5.13

---
+++ src/thinkinginjava/chapter05/exercise13/Cup.java
@@ -0,0 +1,14 @@
+package thinkinginjava.chapter05.exercise13;
+
+public class Cup {
+
+    Cup(int marker) {
+        System.out.println("Cup(" + marker + ")");
+    }
+
+    void f(int marker) {
+        System.out.println("f(" + marker + ")");
+    }
+}
+
+
