12c5299f311fcb2f1020c2d7be4224c7a29c9e5e
Sergei Tarasov <sm.tarasov@gmail.com>
2017-11-24 23:00:24 +0200

    Added exercise 6.2

---
+++ src/thinkinginjava/chapter06/exercise02/Main.java
@@ -0,0 +1,19 @@
+package thinkinginjava.chapter06.exercise02;
+
+/*
+ * Chapter 6. Exercise 2. Difficult - 1/10.
+ *
+ * Take the code fragments in this section and turn them into a program,
+ * and verify that collisions do in fact occur.
+ */
+
+import thinkinginjava.chapter06.exercise02.pkg.*;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Vector vector = new Vector();           // Error:  java: reference to Vector is ambiguous
+    }
+}
---
+++ src/thinkinginjava/chapter06/exercise02/pkg/Vector.java
@@ -0,0 +1,5 @@
+package thinkinginjava.chapter06.exercise02.pkg;
+
+public class Vector {
+
+}
