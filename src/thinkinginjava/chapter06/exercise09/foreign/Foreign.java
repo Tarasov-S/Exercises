d6303901b606ae64a3307808f4c112df618637c3
Sergei Tarasov <sm.tarasov@gmail.com>
2017-11-28 23:47:03 +0200

    Added exercise 6.9

---
+++ src/thinkinginjava/chapter06/exercise09/foreign/Foreign.java
@@ -0,0 +1,18 @@
+package thinkinginjava.chapter06.exercise09.foreign;
+
+/*
+ * Chapter 6. Exercise 9. Difficult - 2/10.
+ *
+ * Create the following file in the access/local directory (presumably in your CLASSPATH):
+ */
+
+import thinkinginjava.chapter06.exercise09.local.*;
+
+public class Foreign {
+
+    public static void main(String[] args) {
+        // PackagedClass pc = new PackagedClass();
+        // Error: java: thinkinginjava.chapter06.exercise09.local.PackagedClass is not public in
+        // thinkinginjava.chapter06.exercise09.local; cannot be accessed from outside package
+    }
+}
---
+++ src/thinkinginjava/chapter06/exercise09/local/PackagedClass.java
@@ -0,0 +1,8 @@
+package thinkinginjava.chapter06.exercise09.local;
+
+class PackagedClass {   // must be public
+
+    public PackagedClass() {
+        System.out.println("PackagedClass()");
+    }
+}
